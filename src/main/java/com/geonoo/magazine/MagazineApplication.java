463789dac1b2c4aea9a883b5affe2277193cd00d
geonoo <yung65322136@gmail.com>
2022-06-23 17:12:44 +0900

    cicd, logback
    테스트

--- src/main/java/com/geonoo/magazine/MagazineApplication.java
+++ src/main/java/com/geonoo/magazine/MagazineApplication.java
@@ -1,11 +1,15 @@
 package com.geonoo.magazine;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@PropertySources({ @PropertySource("classpath:application-aws.properties") })
 public class MagazineApplication {
 
     public static void main(String[] args) {
